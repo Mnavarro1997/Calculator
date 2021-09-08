86d2058d6466cea1f034b83fefbda5e3f6e0d363
MNavarro1997 <mnavarro@gteam.es>
2021-09-08 10:48:53 +0200

    correccion.

--- Calcu.test/UnitTest1.cs
+++ Calcu.test/UnitTest1.cs
@@ -8,10 +8,38 @@ namespace Calcu.test
         [TestMethod]
         public void TestSuma()
         {
-            var s = Suma.Sum(1,2);
-            Assert.AreEqual(s, 3);
+            // de la aplicacion "calcu" escojo la clase creada del objeto "Test" e inicio el calcular donde instacio uno nuevo.
+            Calcu.Calc Calcular = new Calcu.Calc();
+
+            //creacion del resultado que se espera
+            int expectedResult = 7;
+            // creacion de los parametros para que se lleve la ejecucion del metodo Suma
+            int result = Calc.Suma(3, 4);
+
+            //utilizacion del AssertEqual para comparar ambos resultados.
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        public void TestSumaFail()
+        {
+            // de la aplicacion "calcu" escojo la clase creada del objeto "Test" e inicio el calcular donde instacio uno nuevo.
+            Calcu.Calc Calcular = new Calcu.Calc();
+
+            //creacion del resultado que se espera
+            int expectedResult = 6;
+            // creacion de los parametros para que se lleve la ejecucion del metodo Suma
+            int result = Calc.Suma(2, 4);
+
+            //utilizacion del AssertEqual para comparar ambos resultados.
+            Assert.AreNotEqual(expectedResult, result);
+
         }
 
+
+        //NO IMPLEMENTO LOS DEMAS POR QUE NO SE SI ESTA BIEN AL 100%
+        //asi que lo dejo comentado para saber cual de las opciones es la correcta o si tengo que cambiar ambas.
+
+        /*
         [TestMethod]
         public void TestSumaFail()
         {
@@ -33,14 +61,30 @@ namespace Calcu.test
         [TestMethod]
         public void TestMulti()
         {
-            var r = Multiplicacion.Mult(3, 3);
-            Assert.AreEqual(9, r);
+            var m = Multiplicacion.Mult(3, 3);
+            Assert.AreEqual(9, m);
         }
         public void TestMultiFail()
         {
-            var r = Multiplicacion.Mult(3, 3);
-            Assert.AreEqual(5, r);
+            var m = Multiplicacion.Mult(3, 3);
+            Assert.AreEqual(5, m);
         }
+<<<<<<< Updated upstream
+=======
+        [TestMethod]
+        public void TestDiv()
+        {
+            var d = Division.Div(9, 3);
+            Assert.AreEqual(3, d);
+        }
+        public void TestDivFail()
+        {
+            var d = Division.Div(9, 3);
+            Assert.AreEqual(3, d);
+        }
+>>>>>>> Stashed changes
 
+    }
+        */
     }
 }
